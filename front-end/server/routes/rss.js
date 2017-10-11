17d0cd26ee97b3356c743d85dc32320f81db6318
vgo <vgo@pwclabs.be>
2017-10-12 00:42:23 +0200

    Fixed bug with empty rss result

--- front-end/server/routes/rss.js
+++ front-end/server/routes/rss.js
@@ -27,19 +27,22 @@ db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
 // Get reports
 router.get('/rss', (req, res) => {
-	Report.find().sort({'timestamp.submission': 'desc'}).limit(100).exec(function(err, reports) {
+    Report.find().sort({'timestamp.submission': 'desc'}).limit(100).exec(function(err, reports) {
         res.set('Content-Type', 'text/xml');
         if (err) res.send(xml('<error>'+err+'</error>'));
-        var xml = xmlify(reports, { root: 'reports', xmlDeclaration: false});
 
-        xml = xml.substring(9, xml.length - 10);
-        xml = xml.replace(/ id=\"([0-9a-f])*\" _v=\"0\"/g,''); // remove collection id and version from mongodb output
-   		console.log(xml);
+        if (!reports.length) {
+            var xml = "<reports/>";
+        } else {
+            var xml = xmlify(reports, { root: 'reports', xmlDeclaration: false});
+            xml = xml.substring(9, xml.length - 10);
+            xml = xml.replace(/ id=\"([0-9a-f])*\" _v=\"0\"/g,''); // remove collection id and version from mongodb output
+        }
 
 
-   		var meta = '<?xml version="1.0" encoding="UTF-8" ?>'
-   		var start = '<rss version="2.0"><channel><title>EEAP reports RSS feed</title><link>http://www.eeapnode.com</link><description>An RSS feed that provides recently published reports</description>';
-   		var end = '</channel></rss>';
+   		  var meta = '<?xml version="1.0" encoding="UTF-8" ?>'
+   		  var start = '<rss version="2.0"><channel><title>EEAP reports RSS feed</title><link>http://www.eeapnode.com</link><description>An RSS feed that provides recently published reports</description>';
+   		  var end = '</channel></rss>';
 
         res.send(meta+start+xml+end);     	
     });
