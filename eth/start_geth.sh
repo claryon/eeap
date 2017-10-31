334f0c58b5b57cb3a07b960974d2ff9eec17b984
Sander Demeester <demeester.sander@gmail.com>
2017-10-31 18:46:40 +0100

    Add debug to the output

--- eth/start_geth.sh
+++ eth/start_geth.sh
@@ -4,4 +4,4 @@ geth="./geth"
 
 address=$(cat genesis.json |sed -n '/"alloc"/{n;p}' |sed 's/"//g' |sed 's/: {//g' |tr -d '\t')
 
-$geth --etherbase "$address" --password <(cat node_config/keystore/key.priv) --unlock "$address" --identity "node" --rpc --rpcaddr "${DOCKERHOST}" --rpcport "8000" --rpccorsdomain "*" --datadir "./node_config" --port "1111" --nodiscover --ipcpath "/tmp/geth_node.ipc" --networkid 7858765456757 --nat "any" --rpcapi="db,eth,net,web3,personal,web3" console
+$geth --etherbase "$address" --password <(cat node_config/keystore/key.priv) --unlock "$address" --identity "node" --rpc --rpcaddr "${DOCKERHOST}" --rpcport "8000" --rpccorsdomain "*" --datadir "./node_config" --port "1111" --nodiscover --ipcpath "/tmp/geth_node.ipc" --networkid 7858765456757 --nat "any" --rpcapi="db,eth,net,web3,personal,web3" -verbosity 6 console
