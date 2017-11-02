ecd0790991b99437008aa3e863f7c181300417d3
Sander Demeester <demeester.sander@gmail.com>
2017-11-02 12:36:38 +0100

    Add support scripts to the eth folder

---
+++ eth/support_scripts/setup_p2p.sh
@@ -0,0 +1,8 @@
+sudo ssh -i key.pem ubuntu@ec2-34-241-156-163.eu-west-1.compute.amazonaws.com -L 1001:localhost:1111 -Nf
+sudo ssh -i key.pem ubuntu@ec2-34-249-167-141.eu-west-1.compute.amazonaws.com -L 1002:localhost:1111 -Nf
+sudo ssh -i key.pem ubuntu@ec2-34-240-130-189.eu-west-1.compute.amazonaws.com -L 1003:localhost:1111 -Nf
+sudo ssh -i key.pem ubuntu@ec2-52-210-4-189.eu-west-1.compute.amazonaws.com -L 1004:localhost:1111 -Nf
+sudo ssh -i key.pem ubuntu@ec2-52-30-88-206.eu-west-1.compute.amazonaws.com -L 1005:localhost:1111 -Nf
+sudo ssh -i key.pem ubuntu@ec2-34-249-114-254.eu-west-1.compute.amazonaws.com -L 1006:localhost:1111 -Nf
+
+
