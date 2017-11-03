a561ef5b13eded30c81454a0cfa6635931dd8ab3
Sander Demeester <demeester.sander@gmail.com>
2017-11-03 11:30:35 +0100

    Add tunnel shutdown and startup to the eth startup script

--- eth/start_geth.sh
+++ eth/start_geth.sh
@@ -2,6 +2,9 @@
 
 geth="./geth"
 
+sh ./support_scripts/kill_tunnel.sh
+sh ./support_scripts/setup_p2p.sh
+
 address=$(cat genesis.json |sed -n '/"alloc"/{n;p}' |sed 's/"//g' |sed 's/: {//g' |tr -d '\t')
 
 $geth --etherbase "$address" --password <(cat node_config/keystore/key.priv) --unlock "$address" --identity "node" --rpc --rpcaddr "${DOCKERHOST}" --rpcport "8000" --rpccorsdomain "*" --datadir "./node_config" --port "1111" --nodiscover --ipcpath "/tmp/geth_node.ipc" --networkid 7858765456757 --nat "any" --rpcapi="db,eth,net,web3,personal,web3" -verbosity 6 console
