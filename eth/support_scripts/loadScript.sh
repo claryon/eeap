c193c2f730d89f3a8291e356bf06ee363bb471b5
Sander Demeester <demeester.sander@gmail.com>
2017-11-06 07:47:31 +0100

    Fix the path of the geth client and add the peer script

--- eth/support_scripts/loadScript.sh
+++ eth/support_scripts/loadScript.sh
@@ -1,2 +1,2 @@
-geth --exec "loadScript('./checkWork.js');" attach /tmp/geth_node.ipc
-
+../geth --exec "loadScript('./checkWork.js');" attach /tmp/geth_node.ipc
+../geth --exec "loadScript('./addPeer.js');" attach /tmp/geth_node.ipc
